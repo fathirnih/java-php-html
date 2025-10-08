74df1152ae6f46076b4f2f4ece9f5547a3edf3d3
Fathir <fathirulfatih29@gmail.com>
2025-10-08 11:02:12 +0700

    change fetch

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Ambil data artikel dari file JSON
-fetch('data.json')
+fetch('data/data.json')
     .then(response => response.json())
     .then(data => {
         // Menampilkan artikel di halaman
